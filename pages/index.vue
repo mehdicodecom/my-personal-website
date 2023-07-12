99f56ae8854bb6196f542da10878f1534322ecd8
Mehdi Rafiei <info@mehdicode.com>
2023-07-12 15:55:49 +0330

    changed padding of home sections

--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,7 @@
     <Title>Mehdi Rafiei | Front-end Developer | Home</Title>
   </Head>
 
-  <div class="flex px-30 mt-60">
+  <div class="flex pl-24 pr-30 mt-60">
     <Home-HelloWorld />
 
     <Home-Circular />
