3e812ce87dce472a81b896b8b5e83fa6b986e02a
Mehdi Rafiei <info@mehdicode.com>
2025-10-06 14:13:46 +0330

    feat: load white bg of logo if logo loaded

--- components/Header/Logo.vue
+++ components/Header/Logo.vue
@@ -4,14 +4,33 @@
     :to="{ path: '/' }"
     class="relative flex items-center"
   >
-    <img src="/imgs/logo.png" class="z-10 w-18" alt="" />
-    <div class="bg-white h-10 w-12 absolute top-6 left-3"></div>
+    <img 
+    v-show="showImg"
+      ref="logoImg"
+      src="/imgs/logo.png" 
+      class="z-10 w-18" 
+      alt="" 
+    />
+    <div 
+      v-if="imageLoaded"
+      class="bg-white h-10 w-12 absolute top-6 left-3"
+    ></div>
   </nuxt-link>
   <!-- End Logo -->
 </template>
 
-<script>
-export default {};
+<script setup>
+const imageLoaded = ref(false)
+const logoImg = ref(null)
+const showImg = ref(false)
+
+onMounted(() => {
+  // Check if image is already loaded
+  showImg.value = true
+  if (logoImg.value?.complete) {
+    imageLoaded.value = true
+  }
+})
 </script>
 
 <style scoped></style>
